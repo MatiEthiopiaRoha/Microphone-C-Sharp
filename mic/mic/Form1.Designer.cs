c350c4330bb9e3a1806e91e3f19d3d5623285fff
Mati Ethiopia <91181733+MatiEthiopiaRoha@users.noreply.github.com>
2021-12-19 22:56:39 +0000

    Add files via upload

---
+++ mic/mic/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace mic
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.stereoAudioWavePainter1 = new NAudio_Plotting.StereoAudioWavePainter();
+            this.SuspendLayout();
+            // 
+            // stereoAudioWavePainter1
+            // 
+            this.stereoAudioWavePainter1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.stereoAudioWavePainter1.CompositingMode = System.Drawing.Drawing2D.CompositingMode.SourceOver;
+            this.stereoAudioWavePainter1.CompositingQuality = System.Drawing.Drawing2D.CompositingQuality.HighQuality;
+            this.stereoAudioWavePainter1.DrawMode = NAudio_Plotting.StereoAudioWavePainter.DisplayMode.Point;
+            this.stereoAudioWavePainter1.ForeColor = System.Drawing.Color.LawnGreen;
+            this.stereoAudioWavePainter1.Gradient = true;
+            this.stereoAudioWavePainter1.GradientColor = System.Drawing.Color.Lime;
+            this.stereoAudioWavePainter1.InterpolationMode = System.Drawing.Drawing2D.InterpolationMode.HighQualityBicubic;
+            this.stereoAudioWavePainter1.Location = new System.Drawing.Point(12, 12);
+            this.stereoAudioWavePainter1.MiddleLineColor = System.Drawing.Color.Green;
+            this.stereoAudioWavePainter1.Name = "stereoAudioWavePainter1";
+            this.stereoAudioWavePainter1.Pinch = NAudio_Plotting.StereoAudioWavePainter.PinchMode.None;
+            this.stereoAudioWavePainter1.PixelOffsetMode = System.Drawing.Drawing2D.PixelOffsetMode.HighQuality;
+            this.stereoAudioWavePainter1.Plot = NAudio_Plotting.StereoAudioWavePainter.Plotmode.PlusMinus;
+            this.stereoAudioWavePainter1.QualityMode = System.Drawing.Drawing2D.SmoothingMode.HighQuality;
+            this.stereoAudioWavePainter1.ShowMidLine = false;
+            this.stereoAudioWavePainter1.Size = new System.Drawing.Size(1240, 570);
+            this.stereoAudioWavePainter1.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(1264, 594);
+            this.Controls.Add(this.stereoAudioWavePainter1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private NAudio_Plotting.StereoAudioWavePainter stereoAudioWavePainter1;
+    }
+}
+
---
+++ mic/mic/Form1.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using NAudio.Wave;
+using NAudio.Wave.SampleProviders;
+
+namespace mic
+{
+    public partial class Form1 : Form
+      
+    {
+
+        private readonly WaveInEvent _audioSource;
+        private readonly BufferedWaveProvider _provider;
+        private readonly WaveOut _player;
+        private int _count;
+        private const int Speed = 1;
+        private float _leftMax, _rightMax;
+        private readonly object _sampleObject;
+        private readonly NotifyingSampleProvider _notify; 
+        public Form1()
+        {
+            InitializeComponent();
+            _audioSource = new WaveInEvent { WaveFormat = new WaveFormat(44100, WaveIn.GetCapabilities(0).Channels) };
+            _audioSource.DataAvailable += _audioSource_DataAvailable ;
+            _provider = new BufferedWaveProvider(new WaveFormat());
+            _player = new WaveOut();
+            _sampleObject = new object();
+            _notify = new NotifyingSampleProvider(_provider.ToSampleProvider());
+            _notify.Sample += DrawAudioWave;
+
+        }
+
+        private void BufferPlay(byte[] recv)
+        {
+if (recv.Length > 0)
+            {
+                _provider.AddSamples(recv, 0, recv.Length);
+            }
+
+        }
+
+        private void StartAudio()
+        {
+            _audioSource.StartRecording();
+            _player.Init(_notify);
+            _player.Play();
+        }
+        void _audioSource_DataAvailable(object sender, WaveInEventArgs e)
+
+        {
+            BufferPlay(e.Buffer);
+        }
+
+        private void StopAudio()
+        {
+            if (_player == null) return;
+            _player.Dispose();
+            _player.Stop();
+            _provider.ClearBuffer();
+
+        }
+
+        protected override void OnLoad(EventArgs e)
+        {
+            base.OnLoad(e);
+            StartAudio();
+        }
+
+        protected override void OnFormClosing(FormClosingEventArgs e)
+        {
+            base.OnFormClosing(e);
+            StopAudio();
+        }
+
+        private void DrawAudioWave(object sender,SampleEventArgs e)
+        {
+            lock (_sampleObject)
+            {
+                if (_count >= Speed)
+                {
+                    stereoAudioWavePainter1.AddLeftRight(_leftMax, _rightMax);
+                    _leftMax = 0;
+                    _rightMax = 0;
+                    _count = 0;
+                }
+                else
+
+                {
+                    if ( Math.Abs(e.Left) + Math.Abs(e.Right) > Math.Abs(_leftMax) + Math.Abs(_rightMax))
+                    {
+                        _leftMax = e.Left;
+                        _rightMax = e.Right;
+                    }
+                    _count++;
+                }
+            }
+        }
+    }
+}
